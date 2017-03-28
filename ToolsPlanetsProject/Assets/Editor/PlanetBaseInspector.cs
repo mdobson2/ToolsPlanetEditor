ef1fb74bc345e38cd923c4d023f0938d3e1036c6
svc-student <svc-student@student.directory.jccc>
2017-03-27 20:12:08 -0500

    Init Commit

---
+++ ToolsPlanetsProject/Assets/Editor/PlanetBaseInspector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlanetBase))]
+public class PlanetBaseInspector : Editor {
+
+    PlanetBase planetBaseReference;
+
+    void OnEnable()
+    {
+        planetBaseReference = (PlanetBase)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        //serializedObject.Update(); //used to update the current gui to values in the inspector
+        EditorGUILayout.ColorField("Main Color: ", planetBaseReference.mainColor);
+        //serializedObject.ApplyModifiedProperties(); //used to update the information stored in the system
+    }
+}
